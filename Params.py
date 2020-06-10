d3a22ffcf9272ba98328d9ffae5aea25c4a37566
EOKruglov <ye.kruglov@yandex.ru>
2020-06-10 17:29:57 +0300

    code rewriting

---
+++ Params.py
@@ -0,0 +1,254 @@
+import cvxpy as cvx
+import numpy as np
+
+
+class Params:
+
+    __J = None
+    __Jz = None
+    __l0 = None
+    __m = None
+    __c = None
+    __s0 = None
+    __omega = None
+    __eps = None
+    __step = None
+    __alpha = None
+
+    __A = None
+    __Bw = None
+    __Bu = None
+
+    __Ad = None
+    __Bdu = None
+    __Bdw = None
+
+    __CL = None
+    __DL = None
+    __CR = None
+    __DR = None
+
+    __B2 = None
+
+    def __init__(self, J, Jz, l0, m, c, s0, omega, eps, step, alpha):
+        self.__J = J
+        self.__Jz = Jz
+        self.__l0 = l0
+        self.__m = m
+        self.__c = c
+        self.__s0 = s0
+        self.__omega = omega
+        self.__eps = eps
+        self.__step = step
+        self.__alpha = alpha
+
+        T = np.sqrt(m * s0)
+        mu0 = (4 * m * (l0 ** 2)) / (J + m * (l0 ** 2))
+        rho0 = (Jz * T * omega) / (J + m * (l0 ** 2))
+        lambda0 = (c * (T ** 2)) / (J + m * (l0 ** 2))
+
+        A0 = np.array([
+            [1, 0, -mu0, 0, 0, 0],
+            [0, 1, mu0, 0, 0, 0],
+            [-0.125, 0.125, 1, 0, 0, 0],
+            [0, 0, 0, 1, 0, mu0],
+            [0, 0, 0, 0, 1, -mu0],
+            [0, 0, 0, 0.125, -0.125, 1]
+        ])
+
+        A1 = np.array([
+            [-eps, eps, 0, -rho0, 0, 0],
+            [eps, -eps, 0, 0, -rho0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [rho0, 0, 0, -eps, eps, 0],
+            [0, rho0, 0, eps, -eps, 0],
+            [0, 0, 0, 0, 0, 0]
+        ])
+
+        A2 = np.array([
+            [-lambda0, lambda0, 0, -eps * omega, eps * omega, 0],
+            [lambda0, -lambda0, 0, eps * omega, -eps * omega, 0],
+            [0, 0, 0, 0, 0, 0],
+            [eps * omega, -eps * omega, 0, -lambda0, lambda0, 0],
+            [-eps * omega, eps * omega, 0, lambda0, -lambda0, 0],
+            [0, 0, 0, 0, 0, 0]
+        ])
+
+        B1 = np.eye(6)
+
+        B2 = np.array([
+            [-4 * mu0, 0, 0, 0],
+            [0, 4 * mu0, 0, 0],
+            [1, 1, 0, 0],
+            [0, 0, 4 * mu0, 0],
+            [0, 0, 0, -4 * mu0],
+            [0, 0, 1, 1]
+        ])
+
+        self.__B2 = B2
+
+        A = np.concatenate([
+            np.concatenate([np.zeros([6, 6]), np.eye(6)], axis=1),
+            np.concatenate([np.linalg.inv(A0) @ A2, np.linalg.inv(A0) @ A1], axis=1)
+        ], axis=0)
+        Bw = np.concatenate([np.zeros([6, 6]), np.linalg.inv(A0) @ B1], axis=0)
+        Bu = np.concatenate([np.zeros([6, 4]), np.linalg.inv(A0) @ B2], axis=0)
+
+        self.__A = A
+        self.__Bw = Bw
+        self.__Bu = Bu
+
+        Ad = np.eye(12) + step * A
+        Bdw = step * Bw
+        Bdu = step * Bu
+
+        self.__Ad = Ad
+        self.__Bdw = Bdw
+        self.__Bdu = Bdu
+
+        CL = np.array([
+            [0, 0, 0, 2 * l0, 0, 1, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, -2 * l0, 1, 0, 0, 0, 0, 0, 0],
+            [-2 * l0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 2 * l0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ])
+        DL = np.zeros([4, 4])
+        CR = np.zeros([4, 12])
+        DR = np.eye(4)
+
+        self.__CL = CL
+        self.__DL = DL
+        self.__CR = CR
+        self.__DR = DR
+
+    def get_A(self):
+        return self.__A
+
+    def get_Bu(self):
+        return self.__Bu
+
+    def get_Bw(self):
+        return self.__Bw
+
+    def get_Ad(self):
+        return self.__Ad
+
+    def get_Bdu(self):
+        return self.__Bdu
+
+    def get_Bdw(self):
+        return self.__Bdw
+
+    def get_CL(self):
+        return self.__CL
+
+    def get_step(self):
+        return self.__step
+
+    def calculate_KC_Continuous(self):
+        nx = self.__A.shape[0]
+        nu = self.__Bu.shape[1]
+
+        Y = cvx.Variable((nx, nx), symmetric=True)
+        Z = cvx.Variable((nu, nx))
+        gamma = cvx.Variable()
+
+        first_constraint_matrix = cvx.bmat([
+            [Y @ self.__A.T + self.__A @ Y + self.__Bu @ Z + Z.T @ self.__Bu.T, self.__Bw],
+            [self.__Bw.T, -np.eye(6)]
+        ])
+        second_constraint_matrix = cvx.bmat([
+            [Y, Y @ self.__CL[0:3:2].T + Z.T @ self.__DL[0:3:2].T],
+            [self.__CL[0:3:2] @ Y + self.__DL[0:3:2] @ Z, self.__alpha ** 2 * gamma * np.eye(2)]
+        ])
+        third_constraint_matrix = cvx.bmat([
+            [Y, Y @ self.__CL[1:4:2].T + Z.T @ self.__DL[1:4:2].T],
+            [self.__CL[1:4:2] @ Y + self.__DL[1:4:2] @ Z, self.__alpha ** 2 * gamma * np.eye(2)]
+        ])
+        fourth_constraint_matrix = cvx.bmat([
+            [Y, Y @ self.__CR[0:1].T + Z.T @ self.__DR[0:1].T],
+            [self.__CR[0:1] @ Y + self.__DR[0:1] @ Z, (1 - self.__alpha) ** 2 * gamma * np.eye(1)]
+        ])
+        fifth_constraint_matrix = cvx.bmat([
+            [Y, Y @ self.__CR[1:2].T + Z.T @ self.__DR[1:2].T],
+            [self.__CR[1:2] @ Y + self.__DR[1:2] @ Z, (1 - self.__alpha) ** 2 * gamma * np.eye(1)]
+        ])
+        sixth_constraint_matrix = cvx.bmat([
+            [Y, Y @ self.__CR[2:3].T + Z.T @ self.__DR[2:3].T],
+            [self.__CR[2:3] @ Y + self.__DR[2:3] @ Z, (1 - self.__alpha) ** 2 * gamma * np.eye(1)]
+        ])
+        seventh_constraint_matrix = cvx.bmat([
+            [Y, Y @ self.__CR[3:4].T + Z.T @ self.__DR[3:4].T],
+            [self.__CR[3:4] @ Y + self.__DR[3:4] @ Z, (1 - self.__alpha) ** 2 * gamma * np.eye(1)]
+        ])
+
+        constraints = [first_constraint_matrix << 0,
+                       second_constraint_matrix >> 0,
+                       third_constraint_matrix >> 0,
+                       fourth_constraint_matrix >> 0,
+                       fifth_constraint_matrix >> 0,
+                       sixth_constraint_matrix >> 0,
+                       seventh_constraint_matrix >> 0,
+                       Y >> 0,
+                       gamma >= np.finfo(float).eps]
+
+        obj = cvx.Minimize(gamma)
+        problem = cvx.Problem(obj, constraints)
+        opt_val = problem.solve(verbose=True, solver='SCS')
+        KC_Continuous = Z.value @ np.linalg.inv(Y.value)
+        return KC_Continuous
+
+
+    def calculate_KC_Discrete(self):
+        nx = self.__A.shape[0]
+        nu = self.__B2.shape[1]
+
+        Y = cvx.Variable((nx, nx), symmetric=True)
+        Z = cvx.Variable((nu, nx))
+        gamma = cvx.Variable()
+
+        first_constraint_matrix = cvx.bmat([
+            [Y, Y @ self.__Ad.T + Z.T @ self.__Bdu.T, np.zeros([12, 6])],
+            [self.__Ad @ Y + self.__Bdu @ Z, Y, self.__Bdw],
+            [np.zeros([6, 12]), self.__Bdw.T, np.eye(6)]
+        ])
+        second_constraint_matrix = cvx.bmat([
+            [Y, Y @ self.__CL[0:3:2].T + Z.T @ self.__DL[0:3:2].T],
+            [self.__CL[0:3:2] @ Y + self.__DL[0:3:2] @ Z, ((self.__alpha ** 2) * gamma) * np.eye(2)]
+        ])
+        third_constraint_matrix = cvx.bmat([
+            [Y, Y @ self.__CL[1:4:2].T + Z.T @ self.__DL[1:4:2].T],
+            [self.__CL[1:4:2] @ Y + self.__DL[1:4:2] @ Z, ((self.__alpha ** 2) * gamma) * np.eye(2)]
+        ])
+        fourth_constraint_matrix = cvx.bmat([
+            [Y, Y @ self.__CR[0:1].T + Z.T @ self.__DR[0:1].T],
+            [self.__CR[0:1] @ Y + self.__DR[0:1] @ Z, (((1 - self.__alpha) ** 2) * gamma) * np.eye(1)]
+        ])
+        fifth_constraint_matrix = cvx.bmat([
+            [Y, Y @ self.__CR[1:2].T + Z.T @ self.__DR[1:2].T],
+            [self.__CR[1:2] @ Y + self.__DR[1:2] @ Z, (((1 - self.__alpha) ** 2) * gamma) * np.eye(1)]
+        ])
+        sixth_constraint_matrix = cvx.bmat([
+            [Y, Y @ self.__CR[2:3].T + Z.T @ self.__DR[2:3].T],
+            [self.__CR[2:3] @ Y + self.__DR[2:3] @ Z, (((1 - self.__alpha) ** 2) * gamma) * np.eye(1)]
+        ])
+        seventh_constraint_matrix = cvx.bmat([
+            [Y, Y @ self.__CR[3:4].T + Z.T @ self.__DR[3:4].T],
+            [self.__CR[3:4] @ Y + self.__DR[3:4] @ Z, (((1 - self.__alpha) ** 2) * gamma) * np.eye(1)]
+        ])
+
+        constraints = [first_constraint_matrix >> 0,
+                       second_constraint_matrix >> 0,
+                       third_constraint_matrix >> 0,
+                       fourth_constraint_matrix >> 0,
+                       fifth_constraint_matrix >> 0,
+                       sixth_constraint_matrix >> 0,
+                       seventh_constraint_matrix >> 0,
+                       Y >> 0,
+                       gamma >= np.finfo(float).eps]
+
+        obj = cvx.Minimize(gamma)
+        problem = cvx.Problem(obj, constraints)
+        opt_val = problem.solve(verbose=True, solver='SCS')
+        KC_Discrete = Z.value @ np.linalg.inv(Y.value)
+        return KC_Discrete
---
+++ sol.py
@@ -0,0 +1,173 @@
+import numpy as np
+
+from scipy.integrate import odeint
+from Params import Params
+
+
+class Solution:
+
+    __end_time = None
+    __start_time = 0
+    __continuous_period = 0.001
+    __KC_Continuous = None
+    __KC_Discrete = None
+    __exp_param = None
+    __param = None
+
+    def __init__(self, end_time, params: Params, exp_param=-0.01, param=1):
+        self.__end_time = end_time
+        self.__exp_param = exp_param
+        self.__param = param
+        self.__KC_Continuous = params.calculate_KC_Continuous()
+        self.__KC_Discrete = params.calculate_KC_Discrete()
+
+    def influence_harmonic(self, t):
+        return np.array([
+            [np.sin(self.__param * t)],
+            [np.sin(self.__param * t)],
+            [np.sin(self.__param * t)],
+            [np.sin(self.__param * t)],
+            [np.sin(self.__param * t)],
+            [np.sin(self.__param * t)]
+        ])
+
+    def influence_dumped(self, t):
+        return 0.1 * np.exp(self.__exp_param * t) * np.array([
+            [0],
+            [0],
+            [np.sin(self.__param * t)],
+            [0],
+            [0],
+            [np.cos(self.__param * t)]
+        ])
+
+    def get_continuous_solution(self, params: Params, is_harmonic: bool):
+        x0 = np.zeros([12, 1])
+        time = np.arange(self.__start_time, self.__end_time + self.__continuous_period, self.__continuous_period)
+        count = time.size
+        z1_cont = np.zeros([2, count])
+        z2_cont = np.zeros([4, count])
+        z1_cont_not_norm = np.zeros([4, count])
+
+        A = params.get_A()
+        Bu = params.get_Bu()
+        Bw = params.get_Bw()
+        CL = params.get_CL()
+
+        z2_cont[:, 0:1] = self.__KC_Continuous @ x0
+        coord = CL @ x0
+        z1_cont_not_norm[:, 0:1] = coord
+        z1_cont[:, 0:1] = np.array([
+            [np.linalg.norm(coord[0:3:2])],
+            [np.linalg.norm(coord[1:4:2])]
+        ])
+
+        def vdp1(y, t, Ac, Bw):
+            dydt = Ac @ y + (Bw @ self.influence_dumped(t)).reshape([12])
+            return [dydt[0], dydt[1], dydt[2], dydt[3], dydt[4], dydt[5], dydt[6], dydt[7], dydt[8], dydt[9],
+                                    dydt[10], dydt[11]]
+
+        def vdp2(y, t, Ac, Bw):
+            dydt = Ac @ y + (Bw @ self.influence_harmonic(t)).reshape([12])
+            return [dydt[0], dydt[1], dydt[2], dydt[3], dydt[4], dydt[5], dydt[6], dydt[7], dydt[8], dydt[9],
+                    dydt[10], dydt[11]]
+
+        if is_harmonic:
+            y = odeint(vdp2, x0.reshape([12]), time, args=(A + Bu @ self.__KC_Continuous, Bw))
+        else:
+            y = odeint(vdp1, x0.reshape([12]), time, args=(A + Bu @ self.__KC_Continuous, Bw))
+
+        for k in range(count - 1):
+            x0_ = np.transpose(y[k+1:k+2])
+            z2_cont[:, k + 1:k + 2] = self.__KC_Continuous @ x0_
+            coord = CL @ x0_
+            z1_cont[:, k + 1:k + 2] = np.array([
+                [np.linalg.norm(coord[0:3:2])],
+                [np.linalg.norm(coord[1:4:2])]
+            ])
+            z1_cont_not_norm[:, k+1:k+2] = coord
+
+        return z1_cont, z2_cont, z1_cont_not_norm
+
+    def get_discrete_solution(self, params: Params, is_harmonic: bool):
+        x0 = np.zeros([12, 1])
+        step = params.get_step()
+        time = np.arange(self.__start_time, self.__end_time + self.__continuous_period, self.__continuous_period)
+        count = time.size
+        z1_disc = np.zeros([2, count])
+        z2_disc = np.zeros([4, count])
+        z1_disc_not_norm = np.zeros([4, count])
+
+        Ad = params.get_Ad()
+        Bdu = params.get_Bdu()
+        Bdw = params.get_Bdw()
+        CL = params.get_CL()
+
+        for k in range(count):
+            z2_disc[:, k:k+1] = self.__KC_Discrete @ x0
+            coord = CL @ x0
+            z1_disc[:, k:k+1] = [
+                [np.linalg.norm(coord[0:3:2])],
+                [np.linalg.norm(coord[1:4:2])]
+            ]
+            z1_disc_not_norm[:, k:k+1] = coord
+            if is_harmonic:
+                x0 = (Ad + Bdu @ self.__KC_Discrete) @ x0 + Bdw @ self.influence_harmonic(time[k])
+            else:
+                x0 = (Ad + Bdu @ self.__KC_Discrete) @ x0 + Bdw @ self.influence_dumped(time[k])
+
+        return z1_disc, z2_disc, z1_disc_not_norm
+
+    def get_solution(self, params: Params, is_harmonic: bool):
+        step = params.get_step()
+        time = np.arange(self.__start_time, self.__end_time + self.__continuous_period, self.__continuous_period)
+        count = time.size
+        z1 = np.zeros([2, count])
+        z2 = np.zeros([4, count])
+        z1_not_norm = np.zeros([4, count])
+        x0 = np.zeros([12, 1])
+
+        A = params.get_A()
+        Bu = params.get_Bu()
+        Bw = params.get_Bw()
+        CL = params.get_CL()
+
+        z2[:, 0: 1] = self.__KC_Discrete @ x0
+        coord = CL @ x0
+
+        z1[:, 0:1] = np.array([
+             [np.linalg.norm(coord[0:3:2])],
+             [np.linalg.norm(coord[1:4:2])]
+        ])
+        z1_not_norm[:, 0:1] = coord
+
+        def vdp1(y, t, _A, _Bwi, _BwKCx0):
+            dydt = A @ y + _Bwi.reshape([12]) + _BwKCx0.reshape([12])
+            return [dydt[0], dydt[1], dydt[2], dydt[3], dydt[4], dydt[5], dydt[6], dydt[7], dydt[8], dydt[9],
+                    dydt[10], dydt[11]]
+
+        for k in range(count - 1):
+            if is_harmonic:
+                y = odeint(vdp1, x0.reshape([12]), [time[k], time[k + 1]],
+                           args=(A, Bw @ self.influence_harmonic(time[k + 1]),
+                           Bu @ self.__KC_Discrete @ x0.reshape([12, 1])))
+            else:
+                y = odeint(vdp1, x0.reshape([12]), [time[k], time[k + 1]],
+                           args=(A, Bw @ self.influence_dumped(time[k + 1]),
+                           Bu @ self.__KC_Discrete @ x0.reshape([12, 1])))
+
+            x0 = np.transpose(y[1:2])
+
+            z2[:, k + 1:k + 2] = self.__KC_Discrete @ x0
+            coord = CL @ x0
+            z1[:, k + 1:k + 2] = np.array([
+                 [np.linalg.norm(coord[0:3:2])],
+                 [np.linalg.norm(coord[1:4:2])]
+            ])
+            z1_not_norm[:, k + 1:k + 2] = coord
+
+        return z1, z2, z1_not_norm
+
+
+
+
